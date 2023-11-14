6c5d5f7a3266693b8d3cd8518d8ad034613a29dc
Elie Zananiri <prisonerjohn@gmail.com>
2023-11-13 22:01:50 -0500

    fix: comment out std::less templates that are causing conflicts

--- libs/dlib/geometry/vector.h
+++ libs/dlib/geometry/vector.h
@@ -1375,42 +1375,42 @@ namespace dlib
 
 }
 
-namespace std
-{
-    /*!
-        Define std::less<vector<T,3> > so that you can use vectors in the associative containers.
-    !*/
-    template<typename T>
-    struct less<dlib::vector<T,3> > : public binary_function<dlib::vector<T,3> ,dlib::vector<T,3> ,bool>
-    {
-        inline bool operator() (const dlib::vector<T,3> & a, const dlib::vector<T,3> & b) const
-        { 
-            if      (a.x() < b.x()) return true;
-            else if (a.x() > b.x()) return false;
-            else if (a.y() < b.y()) return true;
-            else if (a.y() > b.y()) return false;
-            else if (a.z() < b.z()) return true;
-            else if (a.z() > b.z()) return false;
-            else                    return false;
-        }
-    };
-
-    /*!
-        Define std::less<vector<T,2> > so that you can use vector<T,2>s in the associative containers.
-    !*/
-    template<typename T>
-    struct less<dlib::vector<T,2> > : public binary_function<dlib::vector<T,2> ,dlib::vector<T,2> ,bool>
-    {
-        inline bool operator() (const dlib::vector<T,2> & a, const dlib::vector<T,2> & b) const
-        { 
-            if      (a.x() < b.x()) return true;
-            else if (a.x() > b.x()) return false;
-            else if (a.y() < b.y()) return true;
-            else if (a.y() > b.y()) return false;
-            else                    return false;
-        }
-    };
-}
+//namespace std
+//{
+//    /*!
+//        Define std::less<vector<T,3> > so that you can use vectors in the associative containers.
+//    !*/
+//    template<typename T>
+//    struct less<dlib::vector<T,3> > : public binary_function<dlib::vector<T,3> ,dlib::vector<T,3> ,bool>
+//    {
+//        inline bool operator() (const dlib::vector<T,3> & a, const dlib::vector<T,3> & b) const
+//        { 
+//            if      (a.x() < b.x()) return true;
+//            else if (a.x() > b.x()) return false;
+//            else if (a.y() < b.y()) return true;
+//            else if (a.y() > b.y()) return false;
+//            else if (a.z() < b.z()) return true;
+//            else if (a.z() > b.z()) return false;
+//            else                    return false;
+//        }
+//    };
+//
+//    /*!
+//        Define std::less<vector<T,2> > so that you can use vector<T,2>s in the associative containers.
+//    !*/
+//    template<typename T>
+//    struct less<dlib::vector<T,2> > : public binary_function<dlib::vector<T,2> ,dlib::vector<T,2> ,bool>
+//    {
+//        inline bool operator() (const dlib::vector<T,2> & a, const dlib::vector<T,2> & b) const
+//        { 
+//            if      (a.x() < b.x()) return true;
+//            else if (a.x() > b.x()) return false;
+//            else if (a.y() < b.y()) return true;
+//            else if (a.y() > b.y()) return false;
+//            else                    return false;
+//        }
+//    };
+//}
 
 #if defined(_MSC_VER) && _MSC_VER < 1400
 // turn this warning back on
